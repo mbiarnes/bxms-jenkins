5a4c4f9f55192e3c52c6603c8fa8f085cb4156ff
Piotr Szubiakowski <pszubiak@redhat.com>
2017-05-24 09:21:31 +0200

    Fix 2 environmentVariables

--- jobs/init_release.groovy
+++ jobs/init_release.groovy
@@ -2,42 +2,40 @@ job('sample-init_release') {
 
     description('This is release initialization job.')
 
-    preBuildSteps {
-        enviornamentVariables {
+    environmentVariables {
             propertiesFile('${HOME}/brms-64-jenkins-ci.properties')
             keepBuildVariables(true)
-        }
     }
 
 
     label('pvt-static')
 
-    wrappers {
-        preBuildCleanup()
-    }
-
-    scm {
-        git {
-            url('https://code.engineering.redhat.com/gerrit/integration-platform-config.git/')
-            branch('master')
-        }
-    }
-
-    publishers {
-        createJiraIssue {
-            projectKey('IPBRPMS')
-            testDescription('${product_name} ${product_version} ${release_milestone} Release')
-            assignee('mw-prod-ci')
-        }
-    }
-
-    steps {
-        shell('echo "Hello!"')
-
-        progressJiraIssues {
-            jqlSearch('Summary ~ "${product_name} ${product_version} ${release_milestone} Release"')
-            workflowActionName('To Do')
-            comment('Comment')
-        }
-    }
+//    wrappers {
+//        preBuildCleanup()
+//    }
+//
+//    scm {
+//        git {
+//            url('https://code.engineering.redhat.com/gerrit/integration-platform-config.git/')
+//            branch('master')
+//        }
+//    }
+//
+//    publishers {
+//        createJiraIssue {
+//            projectKey('IPBRPMS')
+//            testDescription('${product_name} ${product_version} ${release_milestone} Release')
+//            assignee('mw-prod-ci')
+//        }
+//    }
+//
+//    steps {
+//        shell('echo "Hello!"')
+//
+//        progressJiraIssues {
+//            jqlSearch('Summary ~ "${product_name} ${product_version} ${release_milestone} Release"')
+//            workflowActionName('To Do')
+//            comment('Comment')
+//        }
+//    }
 }
