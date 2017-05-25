c70968258936827426aac50c4177da9b21aa4923
Piotr Szubiakowski <pszubiak@redhat.com>
2017-05-25 08:46:00 +0200

    Fix logging init.

--- jobs/init_release.groovy
+++ jobs/init_release.groovy
@@ -2,11 +2,11 @@ import java.util.logging.Logger
 
 job('sample-init-release') {
 
-    Logger logger = Logger.getLogger('')
+    Logger logger = Logger.getLogger("")
 
     description('This is release initialization job. This job is responsible for preparation of ${HOME}/brms-64-jenkins-ci.properties file')
 
-    label('pvt-static')
+    label("pvt-static")
 
     wrappers {
         preBuildCleanup()
@@ -23,7 +23,7 @@ job('sample-init-release') {
         }
     }
 
-    logger.warn('Add steps')
+    logger.warn("Add steps")
     steps {
         shell('echo "Hello!"')
     }
