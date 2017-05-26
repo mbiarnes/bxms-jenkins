ddf6b859494ae78a60fadc24fa7fc987fc463788
Piotr Szubiakowski <pszubiak@redhat.com>
2017-05-26 10:55:56 +0200

    Adds brew-build job.

---
+++ jobs/brew_build.groovy
@@ -0,0 +1,99 @@
+// Brew build script
+def shellScript = '''
+#Reset the buld_counter and brew_build_url.
+export `grep "build_counter" ${HOME}/${release_prefix}-jenkins-ci.properties`
+export `grep "ip_config_sha" ${HOME}/${release_prefix}-jenkins-ci.properties`
+
+latest_ip_config_sha=`grep "ip.config.sha=" ${release_prefix}.cfg`
+latest_ip_config_sha=${latest_ip_config_sha##*ip.config.sha=}
+latest_ip_config_sha=${latest_ip_config_sha%%,*}
+echo "latest_ip_config_sha=$latest_ip_config_sha"
+echo "$ip_config_sha"
+if [ "$ip_config_sha" != "$latest_ip_config_sha" ];then
+  build_counter=0
+fi
+echo "current build counter:$build_counter"
+if [ $build_counter -gt 3 ]
+then
+echo "The same Brew build has been tried 3 times， Please investigate the root cause."
+exit 1
+fi
+
+#Enable keytab authentication
+kinit -k -t ${HOME}/host-host-8-172-124.host.centralci.eng.rdu2.redhat.com.keytab host/host-8-172-124.host.centralci.eng.rdu2.redhat.com@REDHAT.COM
+
+git clone  https://code.engineering.redhat.com/gerrit/integration-platform-tooling.git ip-tooling
+
+UNBLOCK=1 BREWCHAIN=1 CFG=./${release_prefix}.cfg POMMANIPEXT=brms-bom make -f  Makefile.BRMS bxms-maven-repo-root  2>&1| tee b.log 
+
+brewchain_build_url=`grep 'build: Watching task ID:' b.log`
+brewchain_build_url=${brewchain_build_url##INFO*\\(}
+brewchain_build_url=${brewchain_build_url% *\\)}
+#task_id=${brewchain_build_url##*taskID=}
+
+echo "Brewchain Build URL: $brewchain_build_url"
+
+build_counter=$(($build_counter+1))
+sed -i '/^build_counter=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "build_counter=$build_counter" >>${HOME}/${release_prefix}-jenkins-ci.properties
+#sed -i '/^brew_build_url=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "brew_build_url=$brew_build_url" >>${HOME}/${release_prefix}-jenkins-ci.properties
+
+
+sed -i '/^brewchain_build_url=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "brewchain_build_url=$brewchain_build_url" >>${HOME}/${release_prefix}-jenkins-ci.properties
+#sed -i '/^task_id=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "task_id=$brewchain_build_url" >>${HOME}/${release_prefix}-jenkins-ci.properties
+echo "Congratulation Brew build is triggered!"
+'''
+
+// Creates or updates a free style job.
+job("sample-brew-build") {
+
+    // Sets a description for the job.
+    description("This is Brew chain build job. This job is responsible for initiation of Brew build.")
+
+    // Label which specifies which nodes this job can run on.
+    label("pvt-static")
+
+    // Adds environment variables to the build.
+    environmentVariables {
+
+        // Adds environment variables from a properties file.
+        propertiesFile('${HOME}/brms-64-jenkins-ci.properties')
+
+        // Inject Jenkins build variables and also environment contributors and build variable contributors provided by other plugins.
+        keepBuildVariables(true)
+
+        // Injects Jenkins system variables and environment variables defined as global properties and as node properties.
+        keepSystemVariables(true)
+    }
+
+    // Adds pre/post actions to the job.
+    wrappers {
+
+        // Deletes files from the workspace before the build starts.
+        preBuildCleanup()
+    }
+
+    // Allows a job to check out sources from an SCM provider.
+    scm {
+
+        // Adds a Git SCM source.
+        git {
+
+            // Adds a remote.
+            remote {
+
+                // Sets the remote URL.
+                url("https://code.engineering.redhat.com/gerrit/integration-platform-config.git/")
+            }
+
+            // Specify the branches to examine for changes and to build.
+            branch('${ip_config_branch}')
+        }
+    }
+
+    // Adds build steps to the jobs.
+    steps {
+
+        // Runs a shell script (defaults to sh, but this is configurable) for building the project.
+        shell('shellScript')
+    }
+}
