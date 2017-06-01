7df93d488813277ecb622fe77259aba0d792d768
Piotr Szubiakowski <pszubiak@redhat.com>
2017-06-01 13:32:48 +0200

    New job definition - brew-build-fail-jira.

---
+++ jobs/brew_build_fail_jira.groovy
@@ -0,0 +1,23 @@
+// Creates or updates a free style job.
+job("sample-brew-build-fail-jira") {
+
+    // Sets a description for the job.
+    description("This job is responsible for create/update jira issue after brew build fail.")
+
+    // Label which specifies which nodes this job can run on.
+    label("pvt-static")
+
+    // Adds pre/post actions to the job.
+    wrappers {
+
+        // Deletes files from the workspace before the build starts.
+        preBuildCleanup()
+    }
+
+    // Adds build steps to the jobs.
+    steps {
+
+        // Runs a shell script (defaults to sh, but this is configurable) for building the project.
+        shell('echo "Do Jira ticket.')
+    }
+}
