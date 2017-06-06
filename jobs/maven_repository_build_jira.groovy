389e36b265bcc50494cb7cad5b3e53b188a65dcc
Piotr Szubiakowski <pszubiak@redhat.com>
2017-06-06 12:53:09 +0200

    Fix name in maven-repository-build-jira.

--- jobs/maven_repository_build_jira.groovy
+++ jobs/maven_repository_build_jira.groovy
@@ -1,5 +1,5 @@
 // Creates or updates a free style job.
-job("sample-release-init-jira") {
+job("sample-maven-repository-build-jira") {
 
     // Sets a description for the job.
     description("This job is responsible for create/update jira issue after maven repository build.")
