67d2ec25180b42c43d51f2e785759705cc28e421
Piotr Szubiakowski <pszubiak@redhat.com>
2017-06-08 10:24:25 +0200

    New jobs definitions - pvt-test-*.

---
+++ jobs/pvt_test_bpms.groovy
@@ -0,0 +1,76 @@
+// PVT script.
+String shellScript = '''cd pvt
+/mnt/maven-3.2.3-prod/bin/mvn clean site surefire-report:report -B -Dproduct.config=${bpms_smoketest_cfg}  -Dproduct.version=${product_version}.${release_milestone}
+
+cd generic/
+
+sed -i '/^bpms_pvt_summary_adoc=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "bpms_pvt_summary_adoc=`pwd`/`find . -name 'pvt_handover_summary*.adoc'`">>${HOME}/${release_prefix}-jenkins-ci.properties
+sed -i '/^bpms_pvt_report_html=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "bpms_pvt_report_html=`pwd`/`find . -name 'pvt_report*.html'`">>${HOME}/${release_prefix}-jenkins-ci.properties
+'''
+
+// Creates or updates a free style job.
+job("sample-pvt-test-bpms") {
+
+    // Sets a description for the job.
+    description("This job is responsible for executing product validation tests.")
+
+    // Label which specifies which nodes this job can run on.
+    label("pvt-static")
+
+    // Adds pre/post actions to the job.
+    wrappers {
+
+        // Deletes files from the workspace before the build starts.
+        preBuildCleanup()
+    }
+
+    // Adds environment variables to the build.
+    environmentVariables {
+
+        // Adds environment variables from a properties file.
+        propertiesFile('${HOME}/brms-64-jenkins-ci.properties')
+
+        // Inject Jenkins build variables and also environment contributors and build variable contributors provided by other plugins.
+        keepBuildVariables(true)
+
+        // Injects Jenkins system variables and environment variables defined as global properties and as node properties.
+        keepSystemVariables(true)
+    }
+
+    scm {
+
+        // Adds a Git SCM source.
+        git {
+
+            // Adds a remote.
+            remote {
+
+                // Sets the remote URL.
+                url("https://github.com/project-ncl/pvt")
+            }
+
+            // Adds additional behaviors.
+            extensions {
+
+                // Specifies a local directory (relative to the workspace root) where the Git repository will be checked out.
+                relativeTargetDirectory("pvt")
+            }
+            // Specify the branches to examine for changes and to build.
+            branch('*/master')
+        }
+    }
+
+    // Adds build steps to the jobs.
+    steps {
+
+        // Runs a shell script (defaults to sh, but this is configurable) for building the project.
+        shell(shellScript)
+    }
+
+    // Adds post-build actions to the job.
+    publishers {
+
+        //Archives artifacts with each build.
+        archiveArtifacts('pvt/generic/*.html,pvt/generic/*.adoc')
+    }
+}
