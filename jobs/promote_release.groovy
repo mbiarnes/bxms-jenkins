457c16c83532afb512fe51e0e559e0c7070130d8
Piotr Szubiakowski <pszubiak@redhat.com>
2017-07-03 12:10:39 +0200

    Add parameters to promote-release job.

--- jobs/promote_release.groovy
+++ jobs/promote_release.groovy
@@ -18,7 +18,7 @@ job("${PRODUCT_NAME}-release-pipeline/${PRODUCT_NAME}-promote-release") {
     environmentVariables {
 
         // Adds environment variables from a properties file.
-        propertiesFile('${HOME}/brms-64-jenkins-ci.properties')
+        propertiesFile("\${HOME}/${CI_PROPERTIES_FILE}")
 
         // Inject Jenkins build variables and also environment contributors and build variable contributors provided by other plugins.
         keepBuildVariables(true)
