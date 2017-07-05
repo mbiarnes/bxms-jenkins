77de64fb57023a906d83be5464c804cc0d897176
Piotr Szubiakowski <pszubiak@redhat.com>
2017-07-05 09:42:03 +0200

    Add parameters to start-brew-build job.

--- jobs/start_brew_build.groovy
+++ jobs/start_brew_build.groovy
@@ -1,46 +1,46 @@
 // Init Brew build script.
-def shellScript = '''# Reset the build_counter and brew_build_url.
-export `grep "build_counter" ${HOME}/${release_prefix}-jenkins-ci.properties`
-export `grep "ip_config_sha" ${HOME}/${release_prefix}-jenkins-ci.properties`
+def shellScript = """# Reset the build_counter and brew_build_url.
+export `grep "build_counter" \${HOME}/\${release_prefix}-jenkins-ci.properties`
+export `grep "ip_config_sha" \${HOME}/\${release_prefix}-jenkins-ci.properties`
 
-latest_ip_config_sha=`grep "ip.config.sha=" ${release_prefix}.cfg`
-latest_ip_config_sha=${latest_ip_config_sha##*ip.config.sha=}
-latest_ip_config_sha=${latest_ip_config_sha%%,*}
-echo "latest_ip_config_sha=$latest_ip_config_sha"
-echo "$ip_config_sha"
-if [ "$ip_config_sha" != "$latest_ip_config_sha" ];then
+latest_ip_config_sha=`grep "ip.config.sha=" \${release_prefix}.cfg`
+latest_ip_config_sha=\${latest_ip_config_sha##*ip.config.sha=}
+latest_ip_config_sha=\${latest_ip_config_sha%%,*}
+echo "latest_ip_config_sha=\$latest_ip_config_sha"
+echo "\$ip_config_sha"
+if [ "\$ip_config_sha" != "\$latest_ip_config_sha" ];then
   build_counter=0
 fi
-echo "current build counter:$build_counter"
-if [ $build_counter -gt 3 ]
+echo "current build counter:\$build_counter"
+if [ \$build_counter -gt 3 ]
 then
 echo "The same Brew build has been tried 3 times， Please investigate the root cause."
 exit 1
 fi
 
 #Enable keytab authentication.
-kinit -k -t ${HOME}/host-host-8-172-124.host.centralci.eng.rdu2.redhat.com.keytab host/host-8-172-124.host.centralci.eng.rdu2.redhat.com@REDHAT.COM
+kinit -k -t \${HOME}/host-host-8-172-124.host.centralci.eng.rdu2.redhat.com.keytab host/host-8-172-124.host.centralci.eng.rdu2.redhat.com@REDHAT.COM
 
 git clone  https://code.engineering.redhat.com/gerrit/integration-platform-tooling.git ip-tooling
 
-UNBLOCK=1 BREWCHAIN=1 CFG=./${release_prefix}.cfg POMMANIPEXT=brms-bom make -f  Makefile.BRMS bxms-maven-repo-root  2>&1| tee b.log 
+UNBLOCK=1 BREWCHAIN=1 CFG=./\${release_prefix}.cfg POMMANIPEXT=brms-bom make -f  ${IP_MAKEFILE} ${PRODUCT_ROOT_COMPNENT}  2>&1| tee b.log 
 
 brewchain_build_url=`grep 'build: Watching task ID:' b.log`
-brewchain_build_url=${brewchain_build_url##INFO*\\(}
-brewchain_build_url=${brewchain_build_url% *\\)}
-#task_id=${brewchain_build_url##*taskID=}
+brewchain_build_url=\${brewchain_build_url##INFO*\\(}
+brewchain_build_url=\${brewchain_build_url% *\\)}
+#task_id=\${brewchain_build_url##*taskID=}
 
-echo "Brewchain Build URL: $brewchain_build_url"
+echo "Brewchain Build URL: \$brewchain_build_url"
 
-build_counter=$(($build_counter+1))
-sed -i '/^build_counter=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "build_counter=$build_counter" >>${HOME}/${release_prefix}-jenkins-ci.properties
-#sed -i '/^brew_build_url=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "brew_build_url=$brew_build_url" >>${HOME}/${release_prefix}-jenkins-ci.properties
+build_counter=\$((\$build_counter+1))
+sed -i '/^build_counter=/d' \${HOME}/\${release_prefix}-jenkins-ci.properties && echo "build_counter=\$build_counter" >>\${HOME}/\${release_prefix}-jenkins-ci.properties
+#sed -i '/^brew_build_url=/d' \${HOME}/\${release_prefix}-jenkins-ci.properties && echo "brew_build_url=\$brew_build_url" >>\${HOME}/\${release_prefix}-jenkins-ci.properties
 
 
-sed -i '/^brewchain_build_url=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "brewchain_build_url=$brewchain_build_url" >>${HOME}/${release_prefix}-jenkins-ci.properties
-#sed -i '/^task_id=/d' ${HOME}/${release_prefix}-jenkins-ci.properties && echo "task_id=$brewchain_build_url" >>${HOME}/${release_prefix}-jenkins-ci.properties
+sed -i '/^brewchain_build_url=/d' \${HOME}/\${release_prefix}-jenkins-ci.properties && echo "brewchain_build_url=\$brewchain_build_url" >>\${HOME}/\${release_prefix}-jenkins-ci.properties
+#sed -i '/^task_id=/d' \${HOME}/\${release_prefix}-jenkins-ci.properties && echo "task_id=\$brewchain_build_url" >>\${HOME}/\${release_prefix}-jenkins-ci.properties
 echo "Congratulation Brew build is triggered!"
-'''
+"""
 
 // Creates or updates a free style job.
 job("${PRODUCT_NAME}-release-pipeline/${PRODUCT_NAME}-start-brew-build") {
@@ -55,7 +55,7 @@ job("${PRODUCT_NAME}-release-pipeline/${PRODUCT_NAME}-start-brew-build") {
     environmentVariables {
 
         // Adds environment variables from a properties file.
-        propertiesFile('${HOME}/brms-64-jenkins-ci.properties')
+        propertiesFile("\${HOME}/${CI_PROPERTIES_FILE}")
 
         // Inject Jenkins build variables and also environment contributors and build variable contributors provided by other plugins.
         keepBuildVariables(true)
