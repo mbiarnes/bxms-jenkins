09485b594ecdb929763bc928c7fe5d58b11b565a
Piotr Szubiakowski <pszubiak@redhat.com>
2017-07-06 14:51:18 +0200

    Move common part of jobs definition to template.

--- jobs/maven_repository_build.groovy
+++ jobs/maven_repository_build.groovy
@@ -16,36 +16,6 @@ def jobDefinition = job("${PRODUCT_NAME}-release-pipeline/${PRODUCT_NAME}-maven-
     // Sets a description for the job.
     description("This job is responsible for offline Maven repository build.")
 
-    // Adds environment variables to the build.
-    environmentVariables {
-
-        // Adds environment variables from a properties file.
-        propertiesFile("\${HOME}/${CI_PROPERTIES_FILE}")
-
-        // Inject Jenkins build variables and also environment contributors and build variable contributors provided by other plugins.
-        keepBuildVariables(true)
-
-        // Injects Jenkins system variables and environment variables defined as global properties and as node properties.
-        keepSystemVariables(true)
-
-        // Prepare repository build script
-        if (PRODUCT_NAME == "intpack17") {
-
-            // Adds an environment variable to the build.
-            env("COMBINATION", "fuse63-bxms64")
-        }
-    }
-
-    // Adds pre/post actions to the job.
-    wrappers {
-
-        // Deletes files from the workspace before the build starts.
-        preBuildCleanup()
-
-        // Adds timestamps to the console log.
-        timestamps()
-    }
-
     // Allows to parameterize the job.
     parameters {
 
@@ -170,4 +140,4 @@ def jobDefinition = job("${PRODUCT_NAME}-release-pipeline/${PRODUCT_NAME}-maven-
     }
 }
 
-JobTemplate.addCommonConfiguration(jobDefinition, this)
+JobTemplate.addCommonConfiguration(jobDefinition)
--- src/main/groovy/org/jboss/bxms/jenkins/JobTemplate.groovy
+++ src/main/groovy/org/jboss/bxms/jenkins/JobTemplate.groovy
@@ -1,12 +1,43 @@
 package org.jboss.bxms.jenkins
 
 
-public class JobTemplate {
-    public static void addCommonConfiguration(def job, def dslFactory) {
+class JobTemplate {
+    static void addCommonConfiguration(job) {
+
         job.with {
 
             // Label which specifies which nodes this job can run on.
             label("pvt-static")
+
+            // Adds environment variables to the build.
+            environmentVariables {
+
+                // Adds environment variables from a properties file.
+                propertiesFile("\${HOME}/${CI_PROPERTIES_FILE}")
+
+                // Inject Jenkins build variables and also environment contributors and build variable contributors provided by other plugins.
+                keepBuildVariables(true)
+
+                // Injects Jenkins system variables and environment variables defined as global properties and as node properties.
+                keepSystemVariables(true)
+
+                // Prepare repository build script
+                if (PRODUCT_NAME == "intpack17") {
+
+                    // Adds an environment variable to the build.
+                    env("COMBINATION", "fuse63-bxms64")
+                }
+            }
+
+            // Adds pre/post actions to the job.
+            wrappers {
+
+                // Deletes files from the workspace before the build starts.
+                preBuildCleanup()
+
+                // Adds timestamps to the console log.
+                timestamps()
+            }
         }
     }
 }
