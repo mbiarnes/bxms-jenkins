0d570b0b9d47f90949cae742173326416d6e98c8
Piotr Szubiakowski <pszubiak@redhat.com>
2017-07-18 11:26:33 +0200

    New PVT job.

---
+++ jobs/pvt_test.groovy
@@ -0,0 +1,60 @@
+import org.jboss.bxms.jenkins.JobTemplate
+
+// PVT script.
+String shellScript = '''cd pvt
+/mnt/maven-3.2.3-prod/bin/mvn  clean site surefire-report:report -B -Dproduct.config=${smoketest_cfg}  -Dproduct.version=${product_version}
+
+cd generic/
+
+sed -i '/^pvt_summary_adoc=/d' ${HOME}/${release_prefix}-jenkins-ci.properties
+sed -i '/^pvt_report_html=/d' ${HOME}/${release_prefix}-jenkins-ci.properties
+echo "pvt_summary_adoc=`pwd`/`find . -name 'pvt_handover_summary*.adoc'`">>${HOME}/${release_prefix}-jenkins-ci.properties
+
+echo "pvt_report_html=`pwd`/`find . -name 'pvt_report*.html'`">>${HOME}/${release_prefix}-jenkins-ci.properties
+'''
+
+// Creates or updates a free style job.
+def jobDefinition = job("${PRODUCT_NAME}-release-pipeline/${PRODUCT_NAME}-pvt-test") {
+
+    // Sets a description for the job.
+    description("This job is responsible for executing product validation tests.")
+
+    scm {
+
+        // Adds a Git SCM source.
+        git {
+
+            // Adds a remote.
+            remote {
+
+                // Sets the remote URL.
+                url("https://github.com/project-ncl/pvt")
+            }
+
+            // Adds additional behaviors.
+            extensions {
+
+                // Specifies a local directory (relative to the workspace root) where the Git repository will be checked out.
+                relativeTargetDirectory("pvt")
+            }
+            // Specify the branches to examine for changes and to build.
+            branch('*/master')
+        }
+    }
+
+    // Adds build steps to the jobs.
+    steps {
+
+        // Runs a shell script (defaults to sh, but this is configurable) for building the project.
+        shell(shellScript)
+    }
+
+    // Adds post-build actions to the job.
+    publishers {
+
+        //Archives artifacts with each build.
+        archiveArtifacts('pvt/generic/*.html,pvt/generic/*.adoc')
+    }
+}
+
+JobTemplate.addCommonConfiguration(jobDefinition, CI_PROPERTIES_FILE, PRODUCT_NAME)
