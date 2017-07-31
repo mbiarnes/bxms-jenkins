60b29cf5d35f18be8f7505c67a9095748e577094
Piotr Szubiakowski <pszubiak@redhat.com>
2017-07-31 12:06:11 +0200

    New BPMS_DELIVERABLE_LIST_FILE property in stage brew build job.

--- jobs/stage_brew_build.groovy
+++ jobs/stage_brew_build.groovy
@@ -18,7 +18,7 @@ if (PRODUCT_NAME == "intpack-fuse63-bxms64") {
     command = 'sed -e \'s=rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/jboss-brms=download.devel.redhat.com/devel/candidates/BRMS=g\' -e \'s=rcm-guest.app.eng.bos.redhat.com/rcm-guest/staging/jboss-bpmsuite=download.devel.redhat.com/devel/candidates/BPMS=g\' brms-64-deliverable-list-staging.properties >> brms-64-deliverable-list.properties'
 
     mavenToStageBpmsCommand = 'ip-tooling/maven-to-stage.py --version=${product_artifact_version} --override-version ${product_version} \\\n' +
-            '   --deliverable ${release_prefix}-release/${release_prefix}-deliverable.properties --maven-repo ${maven_repo_url} \\\n' +
+            "   --deliverable \${release_prefix}-release/${BPMS_DELIVERABLE_LIST_FILE}-deliverable.properties --maven-repo \${maven_repo_url} \\\n" +
             '   --output ${bpms_product_name}-${product_version}.${release_milestone}\\\n' +
             '   --release-url=${rcm_stage_base}/jboss-bpmsuite/${bpms_product_name}-${product_version}.${release_milestone} \\\n' +
             '   --output-deliverable-list ${HOME}/${release_prefix}-deliverable-list-staging.properties'
@@ -44,7 +44,6 @@ ip-tooling/maven-to-stage.py --version=\${product_artifact_version} --override-v
    --output \${brms_product_name}-\${product_version}.\${release_milestone} \\
    --release-url=\${rcm_stage_base}/jboss-brms/\${brms_product_name}-\${product_version}.\${release_milestone} --output-deliverable-list \${HOME}/\${release_prefix}-deliverable-list-staging.properties
    
-
 ${mavenToStageBpmsCommand}
 
 cp \${HOME}/\${release_prefix}-deliverable-list.properties \${release_prefix}-deliverable-list-staging.properties
