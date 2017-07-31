9c20802ed65ac507c01e9bfee8f42e9653f728e3
Piotr Szubiakowski <pszubiak@redhat.com>
2017-07-31 16:17:26 +0200

    Adjust Jira updater.

--- jobs/update_product_jira.groovy
+++ jobs/update_product_jira.groovy
@@ -1,8 +1,9 @@
 import org.jboss.bxms.jenkins.JobTemplate
 
 // Update product JIRA script
-String shellScript = '''echo "The product version is $product_version and the release milestone is $release_milestone."
-python ./ip-tooling/release-ticketor.py --headless $product_version.GA $cutoff_date $product_version.$release_milestone 2>&1 | tee /tmp/release-ticketor-output
+def shellScript = '''echo "The product version is $product_version and the release milestone is $release_milestone."
+ssh anstephe@dev138.mw.lab.eng.bos.redhat.com python ./integration-platform-tooling/release-ticketor.py --headless $product_version.GA $cutoff_date $product_version.$release_milestone 2>&1 | tee /tmp/release-ticketor-output
+
 sed -i '/^resolve_issue_list=/d' ${HOME}/${release_prefix}-jenkins-ci.properties
 echo "resolve_issue_list="`cat /tmp/release-ticketor-output | grep https://url.corp.redhat.com` >>${HOME}/${release_prefix}-jenkins-ci.properties
 '''
