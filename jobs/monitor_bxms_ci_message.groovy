45d0448ded4da32a20fc55e89de347311f3cbaea
Yicheng Sha <yisha@redhat.com>
2017-08-22 10:26:20 +0800

    create a job-dsl to monitor ci message
    IPBRPMS-2317
    
    Change-Id: I7f9494ecc4ee03101ff66bab2a66254bfd58e8b9

---
+++ jobs/monitor_bxms_ci_message.groovy
@@ -0,0 +1,35 @@
+// This job-DSL createsa a job that monitor the bxms ci message
+job('common/monitoring-bxms-CI-message'){
+  description("This DSL generates a job that monitor bxms ci message")
+
+  /*environmentVariables {
+      propertiesFile('${HOME}/${release_prefix}-jenkins-ci.properties')
+      keepSystemVariables(true)
+      keepBuildVariables(true)
+  }*/
+
+  // both tag and label will trigger the job
+  triggers{
+    ciBuildTrigger {
+        selector("label='bxms-ci'")
+        providerName('default')
+    }
+  }
+
+  // print message
+  steps{
+      shell('''echo "I am trigger by a CI message"
+echo "$CI_MESSAGE"''')
+    }
+
+   /* send irc message. Currently connection failed
+   publishers {
+        irc {
+            	channel('#prod-bxms')
+            	setNotifyOnBuildStarts(true)
+            	strategy('ALL')
+            	notificationMessage('Default')
+        	}
+        }*/
+        
+}
