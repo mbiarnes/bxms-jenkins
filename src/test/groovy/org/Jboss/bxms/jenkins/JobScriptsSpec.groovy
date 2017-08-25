159b3a28574b7632bf8c537e7068376f73be61af
Yicheng Sha <yisha@redhat.com>
2017-08-25 13:41:06 +0800

    IPBRPMS-2321 Create a simple gradle test to test all dsl files locally
    Change-Id: Ia3a1668a8ae493a11e90e5100d872c77f496c735

---
+++ src/test/groovy/org/Jboss/bxms/jenkins/JobScriptsSpec.groovy
@@ -0,0 +1,108 @@
+package org.Jboss.bxms.jenkins
+
+import groovy.io.FileType
+import hudson.model.Item
+import hudson.model.View
+import javaposse.jobdsl.dsl.DslScriptLoader
+import javaposse.jobdsl.dsl.GeneratedItems
+import javaposse.jobdsl.dsl.GeneratedJob
+import javaposse.jobdsl.dsl.GeneratedView
+import javaposse.jobdsl.dsl.JobManagement
+import javaposse.jobdsl.plugin.JenkinsJobManagement
+import jenkins.model.Jenkins
+import org.junit.ClassRule
+import org.jvnet.hudson.test.JenkinsRule
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Tests that all dsl scripts in the jobs directory will compile. All config.xml's are written to build/debug-xml.
+ */
+class JobScriptsSpec extends Specification {
+
+    // add in a sample environment for testing
+    def enviMap = ['PRODUCT_NAME':'bxms70', 'CI_PROPERTIES_FILE':'brms-jenkins-ci.properties', 'IP_CONFIG_FILE':'brms.cfg',
+                   'IP_MAKEFILE':'Makefile.BRMS', 'PRODUCT_ROOT_COMPNENT':'bxms-maven-repo-root', 'BPMS_DELIVERABLE_LIST_FILE':'bpmsuite-deliverable.properties',
+                   'REPOSITORY_BUILDER_SCRIPT':'regen_bxms_repo_builder.sh'
+    ]
+
+    @Shared
+    @ClassRule
+    private JenkinsRule jenkinsRule = new JenkinsRule()
+
+    @Shared
+    private File outputDir = new File('./build/debug-xml')
+
+    def setupSpec() {
+        outputDir.deleteDir()
+    }
+
+    @Unroll
+    void 'test script #file.name'(File file) {
+
+        given:
+        JobManagement jm = new JenkinsJobManagement(System.out, enviMap, new File('.'))
+
+        when:
+        GeneratedItems items = new DslScriptLoader(jm).runScript(file.text)
+        writeItems items
+
+        then:
+        noExceptionThrown()
+
+        where:
+        file << jobFiles
+    }
+
+    /**
+     * Write the config.xml for each generated job and view to the build dir.
+     */
+    void writeItems(GeneratedItems items) {
+        Jenkins jenkins = jenkinsRule.jenkins
+
+        items.jobs.each { GeneratedJob generatedJob ->
+            String jobName = generatedJob.jobName
+            Item item = jenkins.getItemByFullName(jobName)
+            String text = new URL(jenkins.rootUrl + item.url + 'config.xml').text
+            writeFile new File(outputDir, 'jobs'), jobName, text
+        }
+
+        items.views.each { GeneratedView generatedView ->
+            String viewName = generatedView.name
+            View view = jenkins.getView(viewName)
+            String text = new URL(jenkins.rootUrl + view.url + 'config.xml').text
+            writeFile new File(outputDir, 'views'), viewName, text
+        }
+    }
+
+    /**
+     * Write a single XML file, creating any nested dirs.
+     */
+    void writeFile(File dir, String name, String xml) {
+        List tokens = name.split('/')
+        File folderDir = tokens[0..<-1].inject(dir) { File tokenDir, String token ->
+            new File(tokenDir, token)
+        }
+        folderDir.mkdirs()
+
+        File xmlFile = new File(folderDir, "${tokens[-1]}.xml")
+        xmlFile.text = xml
+    }
+
+    static List<File> getJobFiles() {
+        List<File> files = []
+        new File('jobs').eachFileRecurse(FileType.FILES) {
+            if (it.name.endsWith('.groovy')) {
+                files << it
+            }
+        }
+
+        // Put the release pipeline dsl to the first so that it will be executed first
+        def pick = files.find{it.name == 'release_pipeline_folder.groovy'}
+        def sorted_files = files.minus(pick)
+        sorted_files.add(0,pick)
+        return sorted_files
+    }
+}
+
