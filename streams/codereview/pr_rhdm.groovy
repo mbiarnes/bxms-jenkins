820a9abb36e2e6617d96c3cfdc4630f0677c38d7
Ryan Zhang <rzhang@redhat.com>
2017-11-14 13:53:29 +0800

    Fix PR job
    Change-Id: I4f9f4a2bbd83b4f8f407b8d60e99e06ae7e0ef9b

--- streams/codereview/pr_rhdm.groovy
+++ streams/codereview/pr_rhdm.groovy
@@ -1,16 +1,15 @@
 def shell_script = """wget http://git.app.eng.bos.redhat.com/git/integration-platform-tooling.git/plain/jssecacerts
 export _KEYSTORE=`pwd`/jssecacerts
-export MAVEN_OPTS="-Djavax.net.ssl.trustStore=${_KEYSTORE} -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=jks -Djavax.net.ssl.keyStore=${_KEYSTORE} -Djavax.net.ssl.keyStorePassword=changeit -Djavax.net.ssl.keyStoreType=jks -Xms512m -Xmx3096m -XX:MaxPermSize=1024m -Dgwt-plugin.localWorkers='3' -XX:+UseConcMarkSweepGC -XX:-UseGCOverheadLimit"
+export MAVEN_OPTS="-Djavax.net.ssl.trustStore=\${_KEYSTORE} -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=jks -Djavax.net.ssl.keyStore=\${_KEYSTORE} -Djavax.net.ssl.keyStorePassword=changeit -Djavax.net.ssl.keyStoreType=jks -Xms512m -Xmx3096m -XX:MaxPermSize=1024m"
 export M3_HOME=/jboss-prod/tools/maven-3.3.9-prod
-export PATH=$M3_HOME/bin:$PATH
+export PATH=\$M3_HOME/bin:\$PATH
 mvn  -DdependencyManagement=org.jboss.brms.component.management:brms-dependency-management-all:7.0.0.redhat-SNAPSHOT \\
-\t-Denforce-skip=false -Dfull=true -DoverrideTransitive=false -Dproject.meta.skip=true \\
-    -DpropertyManagement=org.jboss.brms.component.management:brms-dependency-management-all:7.0.0.redhat-SNAPSHOT \\
+\t -DoverrideTransitive=false -DpropertyManagement=org.jboss.brms.component.management:brms-dependency-management-all:7.0.0.redhat-SNAPSHOT \\
     -Drepo-reporting-removal=true -Dversion.override=7.0.0 -Dversion.suffix=redhat-SNAPSHOT -Dversion.suffix.snapshot=true \\
     -DversionOverride=true -DversionSuffixSnapshot=true -Dvictims.updates=offline -B -Dmaven.repo.local=/jboss-prod/m2/bxms-dev-repo -s /jboss-prod/m2/bxms-dev-repo-settings.xml clean package
 """
 job('rhdm_codereview'){
-    description("Monitor the code change in rhap_common")
+    description("Monitor the code change in rhdm")
 
     parameters {
 
