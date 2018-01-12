e84773c29ef84c19be9b9fd61a5158a3e84b08e6
Yukin Chan <yuqchen@redhat.com>
2018-01-12 13:09:31 +0800

    Fix typo
    Change-Id: I5eabfc70dd6c1eabded85c4b8831edde756d4b6c

--- jobs/locate_import_list.groovy
+++ jobs/locate_import_list.groovy
@@ -120,7 +120,7 @@ kinit -k -t \${HOME}/bxms-release.keytab bxms-release/prod-ci@REDHAT.COM
 echo "Scanning missing artifact..."
 ip-tooling/MEAD_check_artifact.sh \$brew_tag /jboss-prod/m2/\${jenkins_cache_repo} 2>&1 | tee /tmp/mead_check.log
 # echo "`tail -n 5 /tmp/mead_check.log`" > /tmp/mead_check.log # For debug purpose
-sed -ni "/MISSING/p" mead_check.log
+sed -ni "/MISSING/p" /tmp/mead_check.log
 sed -i "/redhat-/d" /tmp/mead_check.log
 sed -i "/SNAPSHOT/d" /tmp/mead_check.log
 importToMeadFromLog /tmp/mead_check.log
@@ -137,7 +137,7 @@ def jobDefinition = job("${RELEASE_CODE}-locate-import-list") {
 
         // Runs a shell script (defaults to sh, but this is configurable) for building the project.
         shell(shellScript)
-        
+
     }
     publishers {
         postBuildTask {
