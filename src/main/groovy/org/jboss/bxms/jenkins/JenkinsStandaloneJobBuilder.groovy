78b9495a49028d1a65a05cce7bcddbf51b8548cc
Ryan Zhang <rzhang@redhat.com>
2018-03-07 18:37:04 +0800

    Patch MEAD_simulator.sh to avoid 'hudson archive and actions no need for nightly build
    Change-Id: Ifea753f32fde9740f7fe4519260b18d79b033631

--- src/main/groovy/org/jboss/bxms/jenkins/JenkinsStandaloneJobBuilder.groovy
+++ src/main/groovy/org/jboss/bxms/jenkins/JenkinsStandaloneJobBuilder.groovy
@@ -48,14 +48,18 @@ class JenkinsStandaloneJobBuilder {
                 String shellScript = """
 unset WORKSPACE
 echo -e "Exec node IP:\${OPENSTACK_PUBLIC_IP}\\n"
+#Only debug purpose
+#cp /jboss-prod/config/rhba-dev.cfg .
+
+#Patch the MEAD_simulator.sh for preventing run hudson archive and deploy check
+sed -i 's/cd "\$_ARCHIVE"/exit \$_ERR;cd "\$_ARCHIVE"/' ip-tooling/MEAD_simulator.sh
 if [ ! -z \${build_date} ]; then
     sed -i "s#-SNAPSHOT#-\${build_date}#g" ${cfg_filename}
 fi
 if [ "${job_type}" == "nightly" ]; then
     sed -i "s#ip.config.sha=#cfg.url.template=file://`pwd`/{0},ip.config.sha=#g" ${cfg_filename}
 fi
-#Only debug purpose
-#cp /jboss-prod/config/MEAD_simulator.sh ip-tooling/MEAD_simulator.sh
+
 MVN_DEP_REPO=nexus-release::default::file://${maven_repo} REPO_GROUP=${repo_group} LOCAL=1 CFG=${_cfg} MVN_LOCAL_REPO=${maven_repo} POMMANIPEXT=\${product_lowercase}-build-bom make DEBUG=\$DEBUG ${section_name}
 """
                 dslFactory.job(release_code + "-" + job_type + "-release-pipeline/y-" + release_code + "-" + section_name ) {
