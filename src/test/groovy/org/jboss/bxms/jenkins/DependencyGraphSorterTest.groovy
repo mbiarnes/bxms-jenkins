0a66e5f237193fd74ce551626f1cfe78f4a1c16b
Marek Winkler <mwinkler@redhat.com>
2019-08-03 21:02:03 +0200

    Update DependencyGraphSorterTests to current nightly configs

--- src/test/groovy/org/jboss/bxms/jenkins/DependencyGraphSorterTest.groovy
+++ src/test/groovy/org/jboss/bxms/jenkins/DependencyGraphSorterTest.groovy
@@ -18,8 +18,8 @@ class DependencyGraphSorterTest {
     public void testRHDMMasterNightlyCfg() {
         Map<String,String[]> packagesMap = DependencyGraphUtils.loadPackageMapFromResource(DependencyGraphSorterTest.class,
                 "/org/jboss/bxms/jenkins/rhdm-master-nightly-dev.cfg")
-        List<String> expected = [ "rhdm-build-bom", "rhba-boms", "rhba-license-builder", "rhdm-maven-repo-root",
-                                  "rhba-common", "rhdm", "rhdm-installer"
+        List<String> expected = [ "rhdm-build-bom", "rhba-license-builder", "rhba-common",
+                                  "rhdm", "rhdm-installer", "rhdm-maven-repo-root"
                                 ]
 
         List<String> sorted = DependencyGraphSorter.kahnTopological(packagesMap)
@@ -30,12 +30,13 @@ class DependencyGraphSorterTest {
     public void testRHPAMMasterNightlyCfg() {
         Map<String,String[]> packagesMap = DependencyGraphUtils.loadPackageMapFromResource(DependencyGraphSorterTest.class,
                 "/org/jboss/bxms/jenkins/rhpam-master-nightly-dev.cfg")
-        List<String> expected = ["rhpam-build-bom", "droolsjbpm-build-bootstrap", "errai", "izpack", "kie-soup",
-                                 "lienzo-core", "kie-uberfire-extensions", "installer-commons", "droolsjbpm-knowledge",
-                                 "lienzo-tests", "drools", "appformer", "jbpm", "optaplanner", "rhba-boms", "droolsjbpm-integration",
-                                 "optashift-employee-rostering", "rhpam-maven-repo-root", "kie-wb-playground", "jbpm-work-items",
-                                 "kie-wb-common", "drools-wb", "jbpm-designer", "optaplanner-wb", "jbpm-wb", "kie-wb-distributions",
-                                 "rhba-license-builder", "rhba-common", "rhpam", "rhpam-installer"]
+        List<String> expected = [ "rhpam-build-bom", "droolsjbpm-build-bootstrap", "errai", "izpack", "kie-soup",
+                                  "lienzo-core", "kie-uberfire-extensions", "installer-commons", "droolsjbpm-knowledge",
+                                  "lienzo-tests", "appformer", "drools", "jbpm", "optaplanner", "rhba-boms", "droolsjbpm-integration",
+                                  "optashift-employee-rostering", "kie-wb-playground", "jbpm-work-items", "openshift-drools-hacep",
+                                  "kie-wb-common", "drools-wb", "jbpm-designer", "optaplanner-wb", "jbpm-wb", "kie-wb-distributions",
+                                  "rhba-license-builder", "rhba-common", "rhpam", "rhpam-installer", "rhpam-maven-repo-root"
+                                ]
 
         List<String> sorted = DependencyGraphSorter.kahnTopological(packagesMap)
         GroovyAssert.assertArrayEquals("Unexpected sort result", expected.toArray(), sorted.toArray())
