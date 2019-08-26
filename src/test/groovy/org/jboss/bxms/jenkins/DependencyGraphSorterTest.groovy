afef546483de99252d488a77e64abad480916cc7
Marek Winkler <mwinkler@redhat.com>
2019-08-26 10:26:53 +0200

    RHPAM nightly: kie-wb-distributions depends on optaweb examples
    Change-Id: I1fac5dbe0abffc85b579ff5219e93fb463aa79f6

--- src/test/groovy/org/jboss/bxms/jenkins/DependencyGraphSorterTest.groovy
+++ src/test/groovy/org/jboss/bxms/jenkins/DependencyGraphSorterTest.groovy
@@ -32,10 +32,11 @@ class DependencyGraphSorterTest {
                 "/org/jboss/bxms/jenkins/rhpam-master-nightly-dev.cfg")
         List<String> expected = [ "rhpam-build-bom", "droolsjbpm-build-bootstrap", "errai", "izpack", "kie-soup",
                                   "lienzo-core", "kie-uberfire-extensions", "installer-commons", "droolsjbpm-knowledge",
-                                  "lienzo-tests", "appformer", "drools", "jbpm", "optaplanner", "rhba-boms", "droolsjbpm-integration",
-                                  "optashift-employee-rostering", "kie-wb-playground", "jbpm-work-items", "openshift-drools-hacep",
-                                  "kie-wb-common", "drools-wb", "jbpm-designer", "optaplanner-wb", "jbpm-wb", "kie-wb-distributions",
-                                  "rhba-license-builder", "rhba-common", "rhpam", "rhpam-installer", "rhpam-maven-repo-root"
+                                  "lienzo-tests", "appformer", "drools", "jbpm", "optaplanner", "kie-jpmml-integration",
+                                  "rhba-boms", "optaweb-employee-rostering", "optaweb-vehicle-routing", "droolsjbpm-integration",
+                                  "kie-wb-playground", "jbpm-work-items", "openshift-drools-hacep", "kie-wb-common", "drools-wb",
+                                  "jbpm-designer", "optaplanner-wb", "jbpm-wb", "kie-wb-distributions", "rhba-license-builder",
+                                  "rhba-common", "rhpam", "rhpam-installer", "rhpam-maven-repo-root"
                                 ]
 
         List<String> sorted = DependencyGraphSorter.kahnTopological(packagesMap)
